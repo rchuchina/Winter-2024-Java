92c20297eac7ed5f6fbb2ef1f7b3edd44427fb72
Rimma <rchuchina@gmail.com>
2024-12-28 17:26:20 -0500

    Class5 and Class6 (with Homework2)  files

---
+++ Winter-2024-java/src/test/java/Class6/BasicGitCommands.java
@@ -0,0 +1,22 @@
+package Class6;
+
+public class BasicGitCommands {
+    public static void main(String[] args) {
+        /**
+         * Basic git commands:
+         * git status
+         *      shows the result after comparing project files in remote(cloud) vs local(laptop/machine)
+         *
+         * git add
+         *      command to add files/folder to be sent to remote
+         *
+         * git commit
+         *      command to write a short-message to mention why are we sending the files to remote
+         *      git commit -m "short message"
+         *
+         * git push
+         *      command to actually sent files (with commit message) in the remote
+         *
+         */
+    }
+}
---
+++ Winter-2024-java/src/test/java/Class6/Homework2.java
@@ -0,0 +1,92 @@
+package Class6;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        String countryName1 = "UNiTeD KInGDom OF GReAt BRiTaIn And NorTHerN irElaND";
+        int result1;
+        /**
+         * Assign value in the result1-variable
+         * if the countryName-length is greater or equals to 6
+         *      assign 10 in result1
+         * otherwise
+         *      assign 20 in result1
+         *
+         * Hint: Ternary operator        */
+        System.out.println("countryName1 -> " + countryName1);
+        System.out.println("\nmethod - length()");
+        int countryName1Length = countryName1.length();
+        System.out.println("countryName1 length -> " + countryName1Length);
+        System.out.println("\nif countryName1Length is greater than or equal to 6");
+        result1 = countryName1Length >= 6 ? 10 : 20;
+        System.out.println("result1 : " + result1);
+        /**
+         * Ignore-cases for all below tasks
+         *
+         * 1. find if countryName2 contains R
+         * 2. find if countryName2 starts with Ki
+         * 3. find if countryName2 and countryName1 are having same value
+         * 4. replace "D" in countryName2 with "SiGMa"
+         */
+
+        /**
+           * Ignore-cases for all below tasks
+           * 1. find if countryName2 contains  R
+         */
+        String countryName2 = "UNiTeD KInGDom OF GReAt BRiTaIn And NorTHerN irElaND";
+        String countryName2Lowercase = countryName2.toLowerCase();
+        System.out.println("\nmethod - toLowerCase()");
+        System.out.println("countryName2 -> " + countryName2);
+        System.out.println("countryName2Lowercase -> " + countryName2Lowercase);
+
+        String pattern = "R";
+        System.out.println("\nmethod - toLowerCase()");
+        System.out.println("pattern -> " + pattern);
+        System.out.println("patternLowerCase -> " + pattern.toLowerCase());
+        boolean isContains_R = countryName2.contains("R");
+        System.out.println("does countryName2 contain 'R' -> " + isContains_R);
+
+        /**
+         * Ignore-cases for all below tasks
+         * 2. find if countryName2 starts with Ki
+         */
+        String countryName2UpperCase = countryName2.toLowerCase();
+        System.out.println("\nmethod - toUpperCase()");
+        System.out.println("countryName2UpperCase -> " + countryName2UpperCase);
+
+         String pattern1  = "Ki";
+         String pattern1UpperCase = pattern1.toUpperCase();
+         System.out.println("\nmethod - toUpperCase()");
+         System.out.println("pattern1 -> " + pattern1);
+         System.out.println("pattern1UpperCase -> " + pattern1UpperCase);
+        boolean isCountryName2StartsWith_Ki = countryName2UpperCase.startsWith(pattern1UpperCase);
+        System.out.println("is countryName2 starts with \"" + pattern1 + "\" (ignoring cases) -> " + isCountryName2StartsWith_Ki);
+
+        /**
+         * Ignore-cases for all below tasks
+         * 3. find if countryName2 and countryName1 are having same value
+         */
+
+
+        System.out.println("\nmethod - equalsIgnoreCase()");
+        System.out.println("countryName1 -> " + countryName1);
+        System.out.println("countryName2 -> " + countryName2);
+        boolean isCountryName1EqualsIgnoreCase_countryName2 =  countryName2.equalsIgnoreCase(countryName2);
+        System.out.println("is countryName1 equalsIgnoreCase countryName2 -> " + isCountryName1EqualsIgnoreCase_countryName2);
+
+
+        /**
+         * Ignore-cases for all below tasks
+         * 4. replace "D" in countryName2 with "SiGMa"
+         */
+
+        System.out.println("\nmethod - replace()");
+        System.out.println("countryName1 -> " + countryName1);
+        String replace_D_SiGMa = countryName1.replace("D", "SiGMa");
+        System.out.println("countryName1 after replace 'D' with 'SiGMa' -> " + replace_D_SiGMa);
+
+    }
+}
+
+
