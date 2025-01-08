671b074a3a1dc9525eafd438c7bfa85750897e63
Rimma <rchuchina@gmail.com>
2025-01-08 01:13:22 -0500

    Class8 homework3 update

--- Winter-2024-java/src/test/java/Class8/Homework3.java
+++ Winter-2024-java/src/test/java/Class8/Homework3.java
@@ -1,5 +1,7 @@
 package Class8;
 
+import java.util.Arrays;
+
 public class Homework3 {
     public static void main(String[] args) {
 
@@ -13,6 +15,45 @@ public class Homework3 {
          *      otherwise, print the name in uppercase
          *
          */
+         // Create an empty array which can store 5-string values
+        String[] Names5 = new String[5];
+        //store 5-string values using index-values, store 5-names in it.
+        System.out.println("Names5 -> " + Arrays.toString(Names5));
+
+        //a) Print the entire array
+        System.out.println("\nFill or Replace values in array using index-numbers");
+        Names5[0] = "Marry";
+        Names5[1] = "Nick";
+        Names5[2] = "Anna";
+        Names5[3] = "Henry";
+        Names5[4] = "Robert";
+        System.out.println("Names5 -> " + Arrays.toString(Names5));
+
+        //b) if index-2-name has more than 5 characters, print the total number of characters present in the name otherwise, print the name in uppercase
+        System.out.println("\nif-else block");
+        System.out.println("Names5[2] = " + Names5[2]);
+        int result;
+        System.out.println("\nmethod - length()");
+        result = Names5[2].length();
+        if (result >=  5) {
+            System.out.println("Names5[2].length = " + result);
+        }
+        else {
+            System.out.println("Names5[2]  = " + Names5[2]);
+        }
+
+        System.out.println("\nif-else block");
+        System.out.println("Names5[4] = " + Names5[4]);
+        int result1;
+        System.out.println("\nmethod - length()");
+        result1 = Names5[4].length();
+              if (result1 >=  5) {
+                   System.out.println("Names5[4].length = " + result1);
+        }
+              else {
+            System.out.println("Names5[4]  = " + Names5[4]);
+        }
+
 
         /**
          * Question-2:
@@ -29,8 +70,22 @@ public class Homework3 {
          *
          */
 
+        String[] sen4word = new String[4];
+        //store 5-string values using index-values, store 5-any 4-words sentence in it.
+        System.out.println("sen4word -> " + Arrays.toString(sen4word));
+        //a) Print the entire array
+        System.out.println("\nFill or Replace values in array using index-numbers");
+        sen4word[0] = "Have a happy life";
+        sen4word[1] = "have a great day";
+        sen4word[2] = "wOrk HaRD paRTy HArdEr";
+        sen4word[3] = "stay humble keep smiling";
+        System.out.println("sen4word -> " + Arrays.toString(sen4word));
 
-
+        System.out.println("\nmethod - split()");
+        String[] splitBy_space = sen4word[0].split(" ");
+        int splitBy_space_length = splitBy_space.length;
+        System.out.println("split by ' ' -> " + Arrays.toString(splitBy_space));
+        System.out.println("Length of splitBy_space -> " + splitBy_space_length);
 
 
     }
