bff13f897f8ae39ab3920f7d68cb2cd57497b6e2
Rimma <rchuchina@gmail.com>
2025-02-05 17:29:32 -0500

    Homework6

--- Winter-2024-java/src/test/java/Class11/Homework6.java
+++ Winter-2024-java/src/test/java/Class11/Homework6.java
@@ -1,12 +1,9 @@
 package Class11;
 
 //import java.util.Arrays;
-
 public class Homework6 {
-   public static void main(String[] args) {
-
-        // there should NOT be any sout-statement in the for-loop or in method
-
+    public static void main(String[] args) {
+     // there should NOT be any sout-statement in the for-loop or in method
         /**
          * Q1: Create a method return string in reverse order
          * "have a GreAT day" -> "day GreAT a have"
@@ -14,32 +11,24 @@ public class Homework6 {
          * "Joy" -> "Joy"
          *
          */
-       String givenString = "have a GreAT day";
-       //String givenString = "Good Morning";
-       //String givenString = "Joy";
-       // Split the string into words
-       String[] words = givenString.split(" ");
-       // Initialize an empty result string
-       String resultString = "";
-       // Iterate through the words array in reverse order
-       for (int i = words.length - 1; i >= 0; i--) {
-           resultString += words[i];
-           // Add a space after each word except the last one
-           if (i != 0) {
-               resultString += " ";
-           }
-       }
-       // Print the result outside the loop
-       System.out.println("Given -> " + givenString);
-       System.out.println("Result -> " + resultString);
 
+        System.out.println("Reversed: " + reverseWords("have a GreAT day"));  // Output: "day GreAT a have"
+        System.out.println("Reversed: " + reverseWords("Good Morning"));     // Output: "Morning Good"
+        System.out.println("Reversed: " + reverseWords("Joy"));              // Output: "Joy"
+        /**
+         * Q2: Create a method to find the total of given int-array
+         * int[] numbers = {1,2,3,4,5}      ->  15
+         * int[] numbers = {1,2,3,1,5,6,8}  ->  26
+         * int[] numbers = {1, 2, -1, -3}   ->  -1
+         */
 
-         /**
-             * Q2: Create a method to find the total of given int-array
-             * int[] numbers = {1,2,3,4,5}      ->  15
-             * int[] numbers = {1,2,3,1,5,6,8}  ->  26
-             * int[] numbers = {1, 2, -1, -3}   ->  -1
-             */
+        int[] numbers1 = {1, 2, 3, 4, 5};
+        int[] numbers2 = {1, 2, 3, 1, 5, 6, 8};
+        int[] numbers3 = {1, 2, -1, -3};
+
+        System.out.println("Total: " + getTotal(numbers1));  // Output: 15
+        System.out.println("Total: " + getTotal(numbers2));  // Output: 26
+        System.out.println("Total: " + getTotal(numbers3));  // Output: -1
 
         /**
          * Q3: Create a method to make abbreviation of given String
@@ -49,6 +38,44 @@ public class Homework6 {
          */
 
 
+        System.out.println("Abbreviation: " + makeAbbreviation("live A life"));         // Output: LAL
+        System.out.println("Abbreviation: " + makeAbbreviation("Make AmErica great Again")); // Output: MAGA
+        System.out.println("Abbreviation: " + makeAbbreviation("gOOd Morning"));        // Output: GM
+    }
 
-   }
+    // Q1: Reverse words in a string
+    public static String reverseWords(String str) {
+        String[] words = str.split(" ");
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]);
+            if (i != 0) {
+                reversed.append(" ");
+            }
+        }
+        return reversed.toString();
+    }
+
+    // Q2: Find total of an int-array
+    public static int getTotal(int[] numbers) {
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    // Q3: Create an abbreviation from a string
+    public static String makeAbbreviation(String str) {
+        String[] words = str.split(" ");
+        StringBuilder abbreviation = new StringBuilder();
+
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                abbreviation.append(Character.toUpperCase(word.charAt(0)));
+            }
+        }
+        return abbreviation.toString();
+    }
 }
