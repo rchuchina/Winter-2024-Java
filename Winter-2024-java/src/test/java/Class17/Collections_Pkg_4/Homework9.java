474c1ad39bfec97ba89e02bafb9626c84224da98
Rimma <rchuchina@gmail.com>
2025-02-11 23:05:14 -0500

    Homework9

--- Winter-2024-java/src/test/java/Class17/Collections_Pkg_4/Homework9.java
+++ Winter-2024-java/src/test/java/Class17/Collections_Pkg_4/Homework9.java
@@ -2,6 +2,8 @@ package Class17.Collections_Pkg_4;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Set;
+import java.util.HashMap;
 
 public class Homework9 {
     public static void main(String[] args) {
@@ -9,20 +11,46 @@ public class Homework9 {
         /**
          * Find the count of color-names in Array
          */
-        String[] colors = {"green", "red", "green" , "blue", "green", "green", "blue", "orange", "red", "blue", "green"};
+        //String[] colors = {"green", "red", "green" , "blue", "green", "green", "blue", "orange", "red", "blue", "green"};
         /*
-            example: {(green-3) , (red-4), (blue-4)}
+          //  example: {(green-3) , (red-4), (blue-4)}
          */
+
+        String[] colors = {"green", "red", "green", "blue", "green", "green", "blue", "orange", "red", "blue", "green"};
+        HashMap<String, Integer> colorCountMap = new HashMap<>();
+
+        for (String color : colors) {
+            if (colorCountMap.containsKey(color)) {
+                colorCountMap.put(color, colorCountMap.get(color) + 1);
+            } else {
+                colorCountMap.put(color, 1);
+            }
+        }
+
+        System.out.println("Color count: " + colorCountMap);
+
+
 
         /**
          * Find the duplicate names from given Array
          */
-        String[] names = {"john", "liya", "kendra" , "john", "doe", "jay", "riya", "rimmi", "liya"};
+        //String[] names = {"john", "liya", "kendra" , "john", "doe", "jay", "riya", "rimmi", "liya"};
         /*
-            example: [john, liya]
+         //   example: [john, liya]
          */
 
 
+        String[] names = {"john", "liya", "kendra", "john", "doe", "jay", "riya", "rimmi", "liya"};
+        Set<String> nameSet = new HashSet<>();
+        Set<String> duplicates = new HashSet<>();
+
+        for (String name : names) {
+            if (!nameSet.add(name)) { // add() returns false if the name is already present
+                duplicates.add(name);
+            }
+        }
+
+        System.out.println("Duplicate names: " + duplicates);
 
     }
 }
